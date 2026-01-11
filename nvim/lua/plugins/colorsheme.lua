0a1f33b09e716b32d454eaae9fd579c0ff052043
guzz <guzz>
2026-01-10 22:01:34 -0300

    initial commit

---
+++ nvim/lua/plugins/colorsheme.lua
@@ -0,0 +1,21 @@
+local opts = {
+  theme = "dark",
+  transparent = true,
+  styles = {
+    type = { bold = true },
+    lsp = { underline = false },
+    match_paren = { underline = true },
+  },
+}
+
+local function config()
+  local plugin = require "no-clown-fiesta"
+  return plugin.load(opts)
+end
+
+return {
+  "aktersnurra/no-clown-fiesta.nvim",
+  priority = 1000,
+  config = config,
+  lazy = false,
+}
